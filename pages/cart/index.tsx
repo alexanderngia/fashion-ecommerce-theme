dfd5e91722c2120de253270757804ccefef74cd1
Alexander <alexanderngia@gmail.com>
2023-04-29 11:42:05 +0700

    update Cart page 11h41 29/4/23

--- pages/cart/index.tsx
+++ pages/cart/index.tsx
@@ -1,6 +1,10 @@
 import LayoutStore from "components/container/layout/store";
-import { ButtonSub } from "components/ui/button";
-import { CardProduct, CardProductItem } from "components/ui/card";
+import { ButtonMain, ButtonSub } from "components/ui/button";
+import {
+  CardProduct,
+  CardProductCart,
+  CardProductItem,
+} from "components/ui/card";
 import Filter from "components/ui/filter";
 import { getProducts } from "lib/productService";
 import { GetStaticProps, NextPage } from "next";
@@ -13,7 +17,8 @@ import { AngleDown } from "@styled-icons/fa-solid/AngleDown";
 
 import styles from "./index.module.scss";
 import { useAppDispatch, useAppSelector } from "redux/hook";
-import { removeFromCart, selectCartList } from "redux/action/cart";
+import { addToCart, removeFromCart, selectCartList } from "redux/action/cart";
+import Divider from "@/components/ui/divider";
 
 interface CartProps {
   product: Products[];
@@ -27,42 +32,69 @@ const Cart: NextPage<CartProps> = () => {
     <>
       <HeaderLayout data={data.nav} layout={layout}></HeaderLayout>
       <LayoutStore>
-        <div className={styles["cart"]}>
-          {cart &&
-            cart?.length > 0 &&
-            cart?.map(
-              (
-                {
-                  idItem,
-                  nameItem,
-                  imgItem,
-                  priceItem,
-                  urlItem,
-                  sizeSelected,
-                  colorSelected,
-                  amount,
-                }: Products,
-                index: number
-              ) => {
-                return (
-                  <CardProductItem
-                    onClick={() =>
-                      dispatch(
-                        removeFromCart({ idItem, colorSelected, sizeSelected })
-                      )
-                    }
-                    key={imgItem + nameItem + index}
-                    href={`store/${urlItem}`}
-                    title={nameItem}
-                    image={imgItem}
-                    sizeItem={sizeSelected}
-                    colorItem={colorSelected}
-                    price={priceItem}
-                    qualityItem={amount}
-                  />
-                );
-              }
-            )}
+        <h1 className={styles["title"]}>Giỏ Hàng</h1>
+        <div className={styles["container"]}>
+          <div className={styles["cart"]}>
+            <h4>ITEMS</h4>
+            <Divider />
+            {cart &&
+              cart?.length > 0 &&
+              cart?.map(
+                (
+                  {
+                    idItem,
+                    nameItem,
+                    imgItem,
+                    priceItem,
+                    urlItem,
+                    sizeSelected,
+                    colorSelected,
+                    amount,
+                  }: Products,
+                  index: number
+                ) => {
+                  return (
+                    <CardProductCart
+                      onClick={() =>
+                        dispatch(
+                          removeFromCart({
+                            idItem,
+                            colorSelected,
+                            sizeSelected,
+                          })
+                        )
+                      }
+                      key={imgItem + nameItem + index}
+                      href={`store/${urlItem}`}
+                      title={nameItem}
+                      image={imgItem}
+                      sizeItem={sizeSelected}
+                      colorItem={colorSelected}
+                      price={priceItem}
+                      qualityItem={amount}
+                    />
+                  );
+                }
+              )}
+          </div>
+          <div className={styles["sidebar"]}>
+            <div className={styles["total"]}>
+              <h4>TỔNG</h4>
+              <p>
+                <strong>20000000 VND</strong>
+              </p>
+            </div>
+            <div className={styles["tax"]}>
+              <p>
+                <strong>Đã bao gồm thuế 10%</strong>
+              </p>
+              <p>Phí ship sẽ được tính khi thanh toán</p>
+            </div>
+            <textarea className={styles["note"]} placeholder="Ghi chú" />
+            <ButtonMain onClick={() => console.log(cart, "cart current")}>
+              Thanh toán
+            </ButtonMain>
+          </div>
         </div>
       </LayoutStore>
     </>
